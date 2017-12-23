49e5843c91ab66a0f05bc5676c1233f524ba1f7d
ohlala <924926395@qq.com>
2017-12-23 17:39:18 +0800

    finish question1

--- tutorial02/test.c
+++ tutorial02/test.c
@@ -134,5 +134,6 @@ static void test_parse() {
 int main() {
     test_parse();
     printf("%d/%d (%3.2f%%) passed\n", test_pass, test_count, test_pass * 100.0 / test_count);
+	getchar();
     return main_ret;
 }
