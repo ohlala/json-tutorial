00e5cd67624ae5dac4d1b63b76f69e815dfbdb18
ohlala <924926395@qq.com>
2018-01-30 22:15:53 +0800

    finish tutorial2  钱

--- tutorial02/leptjson.c
+++ tutorial02/leptjson.c
@@ -2,7 +2,8 @@
 #include <assert.h>  /* assert() */
 #include <stdlib.h>  /* NULL, strtod() */
 #include <stdio.h>
-#include <errno.h>
+#include <errno.h>   /* errno, ERANGE */
+#include <math.h>    /* HUGE_VAL */
 
 #define EXPECT(c, ch)       do { assert(*c->json == (ch)); c->json++; } while(0)
 
@@ -21,7 +22,9 @@ static void lept_parse_whitespace(lept_context* c) {
 //Q1  重构合并 lept_parse_null()、lept_parse_false()、lept_parse_true 为 lept_parse_literal()
 
 static int lept_parse_literal(lept_context* c, lept_value* v, const char* literal, lept_type type) {
-	int i;
+	//注意在 C 语言中，数组长度、索引值最好使用 size_t 类型，而不是 int 或 unsigned。
+	//int i;
+	size_t i;
 	EXPECT(c, literal[0]);
 	for (i = 0;literal[i+1]; i++) {
 		if (c->json[i] != literal[i + 1]) {
@@ -37,14 +40,10 @@ static int lept_parse_literal(lept_context* c, lept_value* v, const char* litera
 #define ISDIGIT1TO9(ch)     ((ch) >= '1' && (ch) <= '9')
 
 static int lept_parse_number(lept_context* c, lept_value* v) {
-	//不清楚这个end干嘛用
-	char* end;
-
     /* \TODO validate number 
 	编写程序检测传入的context是不是满足json数值的要求
 	*/
-	
-	char* p = c->json;
+		char* p = c->json;
 	if (*p == '-') p++;
 	if (*p == '0') p++;
 	else {
@@ -62,17 +61,17 @@ static int lept_parse_number(lept_context* c, lept_value* v) {
 		if (!ISDIGIT(*p)) return LEPT_PARSE_INVALID_VALUE;
 		for (; ISDIGIT(*p); p++);
 	}
-	//??
+	//这里有个end很有趣 表示了每个单词的最后一个字母的后一位  http://en.cppreference.com/w/c/string/byte/strtof
+	//errno 是记录系统的最后一次错误代码。代码是一个int型的值，在errno.h中定义。
 	errno = 0;
-    v->n = strtod(c->json, NULL);
-	if (errno == ERANGE) {
+	v->n = strtod(c->json,NULL);
+	if (errno == ERANGE && (v->n == HUGE_VAL || v->n == -HUGE_VAL)) {
 		return LEPT_PARSE_NUMBER_TOO_BIG;
 	}
-    if (c->json == end)
-        return LEPT_PARSE_INVALID_VALUE;
     c->json = p;
     v->type = LEPT_NUMBER;
     return LEPT_PARSE_OK;
+
 }
 
 static int lept_parse_value(lept_context* c, lept_value* v) {
